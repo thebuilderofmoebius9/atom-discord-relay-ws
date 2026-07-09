e64a67073ee7bbee0f41a2442c58197593e61a71
axeziiezakk <axeziiezakk@gmail.com>
2026-07-09 15:18:35 +0700

    Add Astro GitHub Pages blog

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://thebuilderofmoebius9.github.io',
+  base: '/atom-discord-relay-ws/',
+  output: 'static'
+});
